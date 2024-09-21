b68eb2170ac302319ee68790ca98800427bfb76b
Denis Romodanov <darvenommm@gmail.com>
2024-09-21 23:07:24 +0300

    init: first commit

---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+  sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Window");
+  sf::CircleShape circle(30);
+  circle.setFillColor(sf::Color::Red);
+
+  while (window.isOpen())
+  {
+    sf::Event event;
+
+    while (window.pollEvent(event))
+    {
+      if (event.type == sf::Event::Closed)
+        window.close();
+    }
+
+    window.clear();
+    window.draw(circle);
+    window.display();
+  }
+
+  return EXIT_SUCCESS;
+}
